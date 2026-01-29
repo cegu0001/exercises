15c9c7eb797ad7d5fc2e51f3395bcfc46e598952
Cecilia <cegu0001@stud.kea.dk>
2026-01-29 11:44:59 +0100

    final

--- stensakspapir/rockpaperscissors/js.js
+++ stensakspapir/rockpaperscissors/js.js
@@ -4,6 +4,9 @@ const paper_btn = document.querySelector(".paper");
 const scissors_btn = document.querySelector(".scissors");
 const player1 = document.querySelector("#player1");
 const player2 = document.querySelector("#player2");
+const winText = document.querySelector("#win");
+const loseText = document.querySelector("#lose");
+const drawText = document.querySelector("#draw");
 let userChoice;
 let computerChoice;
 
@@ -17,28 +20,72 @@ function init() {
 }
 function rockClick() {
   // console.log("ROCK");
+  hideTexts();
   userChoice = "rock";
   computerChooses();
 }
 function paperClick() {
   // console.log("PAPER");
+  hideTexts();
   userChoice = "paper";
   computerChooses();
 }
 function scissorsClick() {
   // console.log("SCISSORS");
+  hideTexts();
   userChoice = "scissors";
   computerChooses();
 }
 function computerChooses() {
   console.log("COMPUTER CHOOSES userchoice", userChoice);
-  computerChoice = "scissors";
+  const choices = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  computerChoice = choices[randomNumber];
   handShakeStart();
 }
 function handShakeStart() {
   player1.classList.add("shake");
+  player2.classList.add("shake");
+
+  player1.addEventListener("animationend", handShakeEnd);
 }
 
-// mangler computerchoice - man kan ikke trykke flere gange fordi eventlistener ikke er fjernet
-// lav en eventlistener så der sker noget når animationen stopper
-// lav evt. random number (som i gæt et tal) for at vælge computerchoice
+function handShakeEnd() {
+  player1.classList.remove("shake");
+  player2.classList.remove("shake");
+
+  showHands();
+  determineWinner();
+
+  player1.removeEventListener("animationend", handShakeEnd);
+}
+
+function showHands() {
+  player1.classList.remove("rock", "paper", "scissors");
+  player2.classList.remove("rock", "paper", "scissors");
+
+  player1.classList.add(userChoice);
+  player2.classList.add(computerChoice);
+}
+
+function hideTexts() {
+  winText.classList.add("hidden");
+  loseText.classList.add("hidden");
+  drawText.classList.add("hidden");
+}
+
+function determineWinner() {
+  hideTexts();
+
+  if (userChoice === computerChoice) {
+    drawText.classList.remove("hidden");
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    winText.classList.remove("hidden");
+  } else {
+    loseText.classList.remove("hidden");
+  }
+}
